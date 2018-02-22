937c408d2a0ce8cd9d71dd9d5657c991f88eff61
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2018-02-22 10:46:11 -0500

    first push

--- lab_03/ashokan.cpp
+++ lab_03/ashokan.cpp
@@ -24,7 +24,56 @@ double east_storage(string input_date){
   return level;
 }
 
+
+double mineast(){
+  string date;
+  double eastSt, eastEl, westSt, westEl;
+  ifstream fin("current_reservoir_levels.tsv");
+  if(fin.fail()){
+    cerr << "File cannot be opened for reading." << endl;
+    exit(1); // exit if failed to open the file
+  }
+  string junk; //new string variable
+  getline(fin,junk); //read one line from the file
+  fin >> date >> eastSt >> eastEl >> westSt >> westEl;
+  double min = eastSt;
+  while(fin >> date >> eastSt >> eastEl >> westSt >> westEl){
+    if(eastSt < min){
+      min = eastSt;
+    }
+  }
+  fin.close();
+  return min;
+
+}
+
+double maxeast(){
+  string date;
+  double eastSt, eastEl, westSt, westEl;
+  ifstream fin("current_reservoir_levels.tsv");
+  if(fin.fail()){
+    cerr << "File cannot be opened for reading." << endl;
+    exit(1); // exit if failed to open the file
+  }
+  string junk; //new string variable
+  getline(fin,junk); //read one line from the file
+  fin >> date >> eastSt >> eastEl >> westSt >> westEl;
+  double max = eastSt;
+  while(fin >> date >> eastSt >> eastEl >> westSt >> westEl){
+    if(eastSt > max){
+      max = eastSt;
+    }
+  }
+  fin.close();
+  return max;
+}
+
+
+
 int main(){
   cout << "East storage levels on 01/11/2016 is  " << east_storage("01/11/2016") << endl;
+  cout << "The min East storage is " << mineast() << endl;
+  cout << "The max East storage is " << maxeast() << endl;
   return 0;
+
 }
