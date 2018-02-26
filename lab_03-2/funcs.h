68c2aa36ad30b7ec4290b5b3fcc2b001749914ac
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2018-02-26 08:51:18 -0500

    duplicate of lab_03

---
+++ lab_03-2/funcs.h
@@ -0,0 +1,8 @@
+using namespace std;
+
+void compare_values(double, double);
+double east_storage(string);
+double mineast();
+double maxeast();
+void reverse_order(string, string);
+void compare(string, string);
